2f0d3c6f8db559bff2e6ff3cfb40ecd772621488
Liseni1 <lisenikahapolaarachchi@gmail.com>
2026-02-24 15:50:23 +0530

    Add files via upload
    Added login validation

---
+++ validation.js
@@ -0,0 +1,11 @@
+function validateRegister() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+
+  if (name === "" || email === "") {
+    alert("All fields are required!");
+    return false;
+  }
+}
+
+return true
